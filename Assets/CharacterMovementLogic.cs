e94b9a8dd2e2ec89b1cad9f53559c8c6b760c39b
Somfin <micr0softener@gmail.com>
2014-07-13 17:37:29 +1200

    Got a follow cam working.
    Follow cam is basically functional, but only basically.

---
+++ Assets/CharacterMovementLogic.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovementLogic : MonoBehaviour {
+	private Animator anim;
+	private float directionDampTime = 0.1f;
+	private float Speed = 0.0f;
+	private float h = 0.0f;
+	private float v = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		if (anim.layerCount >= 2) {
+			anim.SetLayerWeight (1, 1);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (anim)
+		{
+			h = Input.GetAxis ("Horizontal");
+			v = Input.GetAxis ("Vertical");
+
+			Speed = new Vector2 (h, v).sqrMagnitude;
+
+			anim.SetFloat("Speed", Speed);
+			anim.SetFloat ("Direction", h, directionDampTime, Time.deltaTime);
+		}
+	}
+}	
---
+++ Assets/ThirdPersonCamLogic.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonCamLogic : MonoBehaviour {
+
+	[SerializeField]
+	private float distanceAway;
+	[SerializeField]
+	private float distanceUp;
+	[SerializeField]
+	private float smooth;
+	[SerializeField]
+	private Transform follow;
+	private Vector3 target;
+	// Use this for initialization
+	void Start () {
+		follow = GameObject.FindWithTag ("CameraController").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void LateUpdate() {
+		target = follow.position + Vector3.up * distanceUp - follow.forward * distanceAway;
+		transform.position = Vector3.Lerp (transform.position, target, Time.deltaTime * smooth);
+		transform.LookAt (follow);
+	}
+}
