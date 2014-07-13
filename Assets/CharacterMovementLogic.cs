0d26c0f0a1ae88cfb67660c2ea844da46cbb2031
Somfin <micr0softener@gmail.com>
2014-07-13 21:46:32 +1200

    Zelda followcam complete.
    Got all of the Zelda cam concept built, from the tutorial.

--- Assets/CharacterMovementLogic.cs
+++ Assets/CharacterMovementLogic.cs
@@ -2,11 +2,24 @@
 using System.Collections;
 
 public class CharacterMovementLogic : MonoBehaviour {
+	[SerializeField]
 	private Animator anim;
+	[SerializeField]
 	private float directionDampTime = 0.1f;
-	private float Speed = 0.0f;
-	private float h = 0.0f;
-	private float v = 0.0f;
+	[SerializeField]
+	private ThirdPersonCamLogic gamecam;
+	[SerializeField]
+	private float directionSpeed = 3.0f;
+	[SerializeField]
+	private float rotationDegreesPerSecond = 120f;
+
+	private float speed = 0.0f;
+	private float direction = 0f;
+	private float horizontal = 0.0f;
+	private float vertical = 0.0f;
+	private AnimatorStateInfo stateInfo;
+
+	private int m_LocomotionID = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -14,19 +27,56 @@ public class CharacterMovementLogic : MonoBehaviour {
 		if (anim.layerCount >= 2) {
 			anim.SetLayerWeight (1, 1);
 		}
+
+		m_LocomotionID = Animator.StringToHash ("Base Layer.Running");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (anim)
 		{
-			h = Input.GetAxis ("Horizontal");
-			v = Input.GetAxis ("Vertical");
+			stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+			horizontal = Input.GetAxis ("Horizontal");
+			vertical = Input.GetAxis ("Vertical");
+			
+			StickToWorldspace(this.transform, gamecam.transform, ref direction, ref speed);
 
-			Speed = new Vector2 (h, v).sqrMagnitude;
+			anim.SetFloat("Speed", speed);
+			anim.SetFloat ("Direction", direction, directionDampTime, Time.deltaTime);
 
-			anim.SetFloat("Speed", Speed);
-			anim.SetFloat ("Direction", h, directionDampTime, Time.deltaTime);
 		}
+	}
+
+	void FixedUpdate() {
+		if (IsInLocomotion() && ((direction >= 0 && horizontal >=0) || (direction < 0 && horizontal < 0))){
+			Vector3 rotationAmount = Vector3.Lerp (Vector3.zero, new Vector3 (0f, rotationDegreesPerSecond * (horizontal < 0f? -1f : 1f), 0f), Mathf.Abs(horizontal));
+			Quaternion deltaRotation = Quaternion.Euler(rotationAmount * Time.deltaTime);
+			this.transform.rotation = (this.transform.rotation * deltaRotation);
+		}
+	}
+
+	public void StickToWorldspace(Transform root, Transform camera, ref float directionOut, ref float speedOut){
+		Vector3 rootDirection = root.forward;
+		Vector3 stickDirection = new Vector3 (horizontal, 0, vertical);
+		speedOut = stickDirection.sqrMagnitude;
+
+		Vector3 CameraDirection = camera.forward;
+		CameraDirection.y = 0.0f;
+		Quaternion referentialShift = Quaternion.FromToRotation (Vector3.forward, CameraDirection);
+
+		Vector3 moveDirection = referentialShift * stickDirection;
+		Vector3 axisSign = Vector3.Cross (moveDirection, rootDirection);
+
+		Debug.DrawRay (new Vector3 (root.position.x, root.position.y + 2f, root.position.z), moveDirection, Color.green);
+		Debug.DrawRay (new Vector3 (root.position.x, root.position.y + 2f, root.position.z), axisSign, Color.red);
+		Debug.DrawRay (new Vector3 (root.position.x, root.position.y + 2f, root.position.z), rootDirection, Color.magenta);
+		Debug.DrawRay (new Vector3 (root.position.x, root.position.y + 2f, root.position.z), stickDirection, Color.blue);
+		float angleRootToMove = Vector3.Angle (rootDirection, moveDirection) * (axisSign.y >= 0 ? -1f : 1f);
+		angleRootToMove /= 180f;
+		directionOut = angleRootToMove * directionSpeed;
+	}
+
+	public bool IsInLocomotion(){
+		return stateInfo.nameHash == m_LocomotionID;;
 	}
 }	
--- Assets/ThirdPersonCamLogic.cs
+++ Assets/ThirdPersonCamLogic.cs
@@ -11,7 +11,15 @@ public class ThirdPersonCamLogic : MonoBehaviour {
 	private float smooth;
 	[SerializeField]
 	private Transform follow;
+	[SerializeField]
+	private Vector3 offset = new Vector3 (1f, 1.5f, 0f);
+	[SerializeField]
+	private float dampingTime = 0.1f;
+
+	private Vector3 velocityCamSmooth = Vector3.zero;
+	private Vector3 lookDir;
 	private Vector3 target;
+
 	// Use this for initialization
 	void Start () {
 		follow = GameObject.FindWithTag ("CameraController").transform;
@@ -23,8 +31,26 @@ public class ThirdPersonCamLogic : MonoBehaviour {
 	}
 
 	void LateUpdate() {
-		target = follow.position + Vector3.up * distanceUp - follow.forward * distanceAway;
-		transform.position = Vector3.Lerp (transform.position, target, Time.deltaTime * smooth);
-		transform.LookAt (follow);
+		Vector3 characterOffset = follow.position + offset;
+
+		lookDir = characterOffset - this.transform.position;
+		lookDir.y = 0;
+		lookDir.Normalize ();
+		Debug.DrawRay (this.transform.position, lookDir, Color.green);
+		
+		target = characterOffset + follow.up * distanceUp - lookDir * distanceAway;
+		Debug.DrawLine (follow.position, target, Color.green);
+		//Debug.DrawRay (follow.position, -1f * follow.forward * distanceAway, Color.red);
+		//Debug.DrawRay (follow.position, Vector3.up * distanceUp, Color.blue);
+		smoothPosition (this.transform.position, target);
+		//transform.position = Vector3.Lerp (transform.position, target, Time.deltaTime * smooth);
+
+
+
+		transform.LookAt (follow.position + offset);
+	}
+
+	private void smoothPosition(Vector3 from, Vector3 to){
+		this.transform.position = Vector3.SmoothDamp (from, to, ref velocityCamSmooth, dampingTime);
 	}
 }
